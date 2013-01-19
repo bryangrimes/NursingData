32034425e57dffdfd26a19182ff712fd0a5d4420
Bryan Grimes <bgrimes@energypluscompany.com>
2013-01-19 15:00:39 -0500

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NurseApp::Application.config.secret_token = '8eef0ff3ef9bd8778269ae2ed0035fe4c3ba9141e94d79d1dcf294ebff06bcb0b89ec760ccfb20b36c6ab3370cee0d30af26055973046d9245da088ebf76d3a8'
